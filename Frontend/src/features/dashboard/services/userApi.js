5112f5a7b50c3e09898830ae3324ef8d6593b07d
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2026-06-24 06:43:54 +0000

    abcde

--- Frontend/src/features/dashboard/services/userApi.js
+++ Frontend/src/features/dashboard/services/userApi.js
@@ -1,23 +1,35 @@
 import api from "../../../app/api"
 
+// // Books
+// export const getMyBooks = async () => {
+//   const response = await api.get("/book/")
+//   return response.data
+// }
+
+// export const getBook = async (bookId) => {
+//   const response = await api.get(`/book/${bookId}/`)
+//   return response.data
+// }
+
+// // Tickets
+// export const getMyTickets = async () => {
+//   const response = await api.get("/ticket/")
+//   return response.data
+// }
+
+// export const createTicket = async ({ subject, description, category, book }) => {
+//   const response = await api.post("/ticket/", { subject, description, category, book })
+//   return response.data
+// }
+
 // Books
 export const getMyBooks = async () => {
   const response = await api.get("/book/")
-  return response.data
-}
-
-export const getBook = async (bookId) => {
-  const response = await api.get(`/book/${bookId}/`)
-  return response.data
+  return response.data.results  
 }
 
 // Tickets
 export const getMyTickets = async () => {
   const response = await api.get("/ticket/")
-  return response.data
-}
-
-export const createTicket = async ({ subject, description, category, book }) => {
-  const response = await api.post("/ticket/", { subject, description, category, book })
-  return response.data
+  return response.data.results  
 }
