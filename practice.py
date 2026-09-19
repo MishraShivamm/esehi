bb75ee713705fe80d44a1f8f390c4da45388316d
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2026-09-19 07:59:03 +0000

    python

---
+++ practice.py
@@ -0,0 +1,12 @@
+# Excerciese 1
+first_name = "Tony";
+last_name = "Stark";
+age = 53 ; 
+height = 1.85 ;
+secret = input("Enter his comic name");
+
+print("My name is" , first_name , last_name ," and my age is" , age ,
+      "and my height is" , height , "AND I'm" , secret);
+
+ # this is f string command:
+print(f"My name is {first_name} {last_name} and my age is {age}and my height is {height} AND I'm {secret}");
