a0b0a44fcc2349ff5374082d8c525dbeeaa477a0
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2026-09-19 07:45:22 +0000

    python

---
+++ code.py
@@ -0,0 +1,20 @@
+print("Hello");
+
+print(1);
+#python is case sensitive language.
+
+# Variables :  they are the container in which we can store any value.
+
+name = "Shivi";
+age =26
+print(name, type(age));
+
+# four types of primitive data types are:
+# int, string, float, boolean
+
+##INPUT
+#command example ( input("Enter your name"))
+
+x = input("Enter name");
+
+print("Hey hey"+ x);
