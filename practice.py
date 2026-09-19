e13012eea057dbd2e2c1694b0479d30177c7c68c
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2026-09-19 11:41:05 +0000

    python

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
-# Excerciese 1
+"""
+# Exercise 1
 first_name = "Tony";
 last_name = "Stark";
 age = 53 ; 
@@ -11,15 +12,15 @@ print("My name is" , first_name , last_name ," and my age is" , age ,
  # this is f string command:
 print(f"My name is {first_name} {last_name} and my age is {age}and my height is {height} AND I'm {secret}");
 
+"""
 
 
-
-#EXCERSCISE 2
+#EXERCISE 2
 """
 write a sum program where there are 2 integers a and b ,
 whose input we'll get from terminal. and calculate those and print it .
 """
-
+"""
 A = int(input ("enter value of A"));
 B = int(input("Enter value of B"));
 
@@ -27,12 +28,12 @@ num = A + B;
 
 print (num);
 
-
+"""
 
 """
 write a subtraction program where there are 2 integers a and b ,
 whose input we'll get from terminal. and calculate those and print it .
-"""
+
 
 Aa = int(input ("enter value of A"));
 Bb = int(input("Enter value of B"));
@@ -40,3 +41,28 @@ Bb = int(input("Enter value of B"));
 numm = Aa - Bb;
 
 print (numm);
+"""
+
+"""
+Exercise 3:
+
+Take price of 3 products as input (eg - 99.8, 23.56, 16.15)
+. print total Bill amount
+. print the average price
+
+Take a superhero name as input & check if it starts with 's' /'S' or not.
+
+"""
+
+product_price1 =  99.8;
+product_price2 = 23.56;
+product_price3 = 16.15;
+
+total_price = product_price1 + product_price2  + product_price3;
+avg_price = ((product_price1 + product_price2  + product_price3)/3);
+
+print (total_price , avg_price);
+
+name = "IronMan";
+print ('s' in name);
+print ('S' in name); 
