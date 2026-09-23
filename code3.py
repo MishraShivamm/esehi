b412c38d7b6aa591f0c87fcfe6a12bed360288c5
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2026-09-23 06:52:39 +0000

    python

--- code3.py
+++ code3.py
@@ -20,5 +20,12 @@ Comparison Operators: Used to compare two values and return a boolean result.
 example: ==, !=, >, <, >=, <=
 
 
+Logical Operators: Used to combine multiple boolean expressions and return 
+a boolean result. example: and, or, not
 
+Conversion Operators: Used to convert a value from one data type to another.
+example: int(), float(), str()
+
+Conditional Statements: Used to execute different blocks of code based on 
+certain conditions. example: if, elif, else
 """
