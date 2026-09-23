50dc60ba975d33d1550f198c0026ddf9d83634d7
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2026-09-23 07:05:30 +0000

    python

--- code3.py
+++ code3.py
@@ -21,7 +21,7 @@ example: ==, !=, >, <, >=, <=
 
 
 Logical Operators: Used to combine multiple boolean expressions and return 
-a boolean result. example: and, or, not
+a boolean result. example: and, or, not 
 
 Conversion Operators: Used to convert a value from one data type to another.
 example: int(), float(), str()
@@ -29,3 +29,12 @@ example: int(), float(), str()
 Conditional Statements: Used to execute different blocks of code based on 
 certain conditions. example: if, elif, else
 """
+age = 24;
+if age >= 18:
+    print("You are eligible to vote.");
+
+elif age < 18:
+    print("You are not eligible to vote.");
+
+else:
+    print("Invalid age.");
