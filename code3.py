bdad0f2cf130cb63506e5c9929ff4987a0b9aee7
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2026-09-23 06:37:14 +0000

    python

---
+++ code3.py
@@ -0,0 +1,24 @@
+"""
+Arithmetic Operation
+
+modulo operator : calculate the remainder of the division of two numbers.
+
+
+
+
+Shourtcuts: Assignment operators: +=, -=, *=, /=, //=, **=, %=
+
+Operator Precedence: The order in which operations are performed in an 
+expression. For example, multiplication and division have higher precedence 
+than addition and subtraction. 
+the most higher precedence is for parentheses () which means 
+whatever is in the parentheses will be calculated first and 
+then the rest of the expression will be calculated.
+
+
+Comparison Operators: Used to compare two values and return a boolean result.
+example: ==, !=, >, <, >=, <=
+
+
+
+"""
