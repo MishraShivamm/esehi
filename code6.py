d5a4065c50b76603c6b07207528eefdbad99ad58
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2026-09-25 10:18:38 +0000

    python

--- code6.py
+++ code6.py
@@ -3,11 +3,11 @@ Function in python is a block of code that is designed to perform a specific tas
 them, and return an output. Functions help in organizing code, making it reusable, and improving readability.
 
 Types of Functions in Python:
- in-built functions: These are the functions that are already defined in Python and can be used directly.
+--> in-built functions: These are the functions that are already defined in Python and can be used directly.
  examples of in-built functions are print(), len(), type(), etc.
- User-defined functions: These are the functions that are defined by the user to perform specific tasks.
+--> User-defined functions: These are the functions that are defined by the user to perform specific tasks.
  examples of user-defined functions are def my_function():, def add_numbers(a, b):, etc.
- Module Functions: These are the functions that are defined in external modules and can be imported and used in 
+--> Module Functions: These are the functions that are defined in external modules and can be imported and used in 
  the code.examples of module functions are math.sqrt(), random.randint(), etc.
 
  
@@ -27,4 +27,10 @@ def gst(price):
     total_price = price + price *0.18
     return total_price
 print(gst(133))
+import math
 
+print(dir(math))  # This will print all the attributes and methods of the math module
+
+from math import sqrt,log2
+print(sqrt(16))  # This will print the square root of 16
+print(log2(16))  # This will print the base-2 logarithm of 16
